e10e451905ae74efed877893df04a1702c79fb00
TanvikaOjha <25je0806@iitism.ac.in>
2026-06-17 22:33:55 +0530

    first commit

---
+++ test/FundMe.t.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+
+
+pragma solidity ^0.8.18;
+
+
+import {Test, console} from 'forge-std/Test.sol';
+import {FundMe} from '../src/FundMe.sol';
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+
+contract FundMeTest is Test{
+    uint256 favNumber = 0;
+    bool greatCourse = false;
+    DeployFundMe deployFundMe;
+    function setUp() external{
+        
+         fundMe = new FundMe();
+         deployFundMe = new DeployFundMe();
+         fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+}
